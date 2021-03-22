8f7d84910e87b9c75a7f628ee611ee2ed468e402
juskis4 <ljuskevicius2002@gmail.com>
2021-03-22 11:28:45 +0100

    Update Model.java

--- src/model/Model.java
+++ src/model/Model.java
@@ -6,10 +6,5 @@ import java.util.ArrayList;
 
 public interface Model extends UnnamedPropertyChangeSubject
 {
-  public ArrayList<Exercise> getAllExercises();
-  public ArrayList<Exercise> getExercises(boolean completed);
-  public Exercise getExercise(String number);
-  public Exercise removeExercise(String number);
-  public void addExercise(Exercise exercise);
-  public Exercise editExercise(String number, Exercise exercise);
+  void
 }
